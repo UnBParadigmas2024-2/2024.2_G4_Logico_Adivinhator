ea9b2c86b6a7692afdb2f3544738c3b4b7a58034
Yudi Yamane <yudiyamane@gmail.com>
2024-12-10 19:38:07 -0300

    Traduz código para português

--- interface.pl
+++ interface.pl
@@ -4,7 +4,7 @@
 menu :- 
     new(Dialog, dialog('Adivinhator')),
     send(Dialog, clear),
-    initial_prompt(Dialog).
+    prompt_inicial(Dialog).
 
 % Referências usadas:
 % https://github.com/UnBParadigmas2023-2/2023.2_G2_Logico_YouTubeRecommendation/blob/main/src/menu.pl
@@ -12,62 +12,64 @@ menu :-
 
 
 % Pode mudar esse comecinho à vontade. Serviu pra ter alguma coisa na tela
-initial_prompt(Dialog) :-
+prompt_inicial(Dialog) :-
     send(Dialog, clear),
     send(Dialog, size, size(800, 600)),
     send(Dialog, background, '#202020'),
 
-    new(Welcome, text('Bem vindo ao Adivinhator')),
+    new(Bemvindo, text('Bem vindo ao Adivinhator')),
     new(Prompt, text('Pense em um animal, clique em \'Próximo\' quando estiver pronto')),
-    send(Welcome, colour, white),
+    send(Bemvindo, colour, white),
     send(Prompt, colour, white),
     new(F, font(screen, bold, 20)),
-    send(Welcome, font(F)),
+    send(Bemvindo, font(F)),
     send(Prompt, font(F)),
 
-    new(Next, button('Pŕoximo', and(message(@prolog, next_question, Dialog)))),
+    new(Proximo, button('Pŕoximo', and(message(@prolog, proxima_pergunta, Dialog)))),
 
-    send(Dialog, append, Welcome),
+    send(Dialog, append, Bemvindo),
     send(Dialog, append, Prompt),
-    send(Dialog, append, Next),
+    send(Dialog, append, Proximo),
     send(Dialog, layout_dialog),
     send(Dialog, open_centered).
 
 
-next_question(Dialog) :-
+proxima_pergunta(Dialog) :-
     send(Dialog, clear),
     send(Dialog, size, size(800, 600)),
     send(Dialog, background, '#202020'),
 
-    send(Dialog, append, new(Buttons, dialog_group(buttons, group))),
+    send(Dialog, append, new(Botoes, dialog_group(buttons, group))),
 
     % TODO: Pergunta hardcoded por enquanto. Tem que chamar uma função pra elaborar uma pergunta.
-    % Algo do tipo get_next_question(Q). Então adiciona ela na tela:
-    % new(Question, text(Q)),
+    % Algo do tipo gera_nova_pergunta(P). Então adiciona ela na tela:
+    % new(Pergunta, text(P)),
 
-    new(Question, text('Uma pergunta muito bem elaborada??')),
-    send(Question, colour, white),
+    new(Pergunta, text('Uma pergunta muito bem elaborada??')),
+    send(Pergunta, colour, white),
     new(F, font(screen, bold, 20)),
-    send(Question, font(F)),
+    send(Pergunta, font(F)),
 
-    new(BtnYes,      button('Sim',     and(message(@prolog, answer_question, Dialog, yes)))),
-    new(BtnDontKnow, button('Não sei', and(message(@prolog, answer_question, Dialog, dontknow)))),
-    new(BtnNo,       button('Não',     and(message(@prolog, answer_question, Dialog, no)))),
+    new(BtSim,    button('Sim',     and(message(@prolog, responder_pergunta, Dialog, sim)))),
+    new(BtNaosei, button('Não sei', and(message(@prolog, responder_pergunta, Dialog, naosei)))),
+    new(BtNao,    button('Não',     and(message(@prolog, responder_pergunta, Dialog, nao)))),
 
-    send(Buttons, append(Question)),
-    send(Buttons, append(BtnYes)),
-    send(Buttons, append(BtnDontKnow)),
-    send(Buttons, append(BtnNo)),
-    send(Buttons, layout_dialog),
+    % TODO: Precisa de um botão de saída, que fecha a aplicação por inteiro
+
+    send(Botoes, append(Pergunta)),
+    send(Botoes, append(BtSim)),
+    send(Botoes, append(BtNaosei)),
+    send(Botoes, append(BtNao)),
+    send(Botoes, layout_dialog),
 
     send(Dialog, layout),
     send(Dialog, open_centered).
 
 
-answer_question(Dialog, A) :-
-    write(A), nl,
+responder_pergunta(Dialog, Resposta) :-
+    write(Resposta), nl,
 
     % TODO: atualizar banco, gerar query e essas coisas
     % Não é bem responsabilidade do pessoal da interface
     
-    next_question(Dialog).
+    proxima_pergunta(Dialog).
